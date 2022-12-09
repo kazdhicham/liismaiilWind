9a0cf7048ec47f32fc4511a668031a984b4fe07d
kacham <kazdhicham@gmal.com>
2022-12-09 08:46:54 +0100

    init v1 liismaiilWind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { AppProps } from 'next/app'
+import '../styles/index.css'
+import SEO from '@/lib/next-seo-config';
+import { NextSeo } from 'next-seo';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import Layout from '@/components/Layout';
+import { InterfaceProvider } from '@/store/contexts/InterfaceContext'
+export default function myApp({ Component, pageProps }: AppProps) {
+
+  return (
+
+    <>
+      <Head>
+        <title>liismaiil /ludique Organistaion and Family tools </title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <NextSeo {...SEO} />
+
+      <Layout>
+        <InterfaceProvider>
+
+          <Component {...pageProps} />
+        </InterfaceProvider>
+      </Layout>
+    </>
+  )
 }
